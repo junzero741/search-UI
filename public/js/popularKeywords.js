6ad4e92ff0cacb37b499b2a3914bcf3d5e81752c
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-03-10 18:07:17 +0900

    Add : 추천검색어 노출 기능 구현
    * 검색어 입력한 뒤 마우스 치워도 추천검색어가 사라지지 않는 버그가 있습니다. 입력한 검색어를 다 지워야 사라지네요.
    * 앞서 구현한 인기 검색 키워드와의 showing toggle을 일치시키데에 어려움이 있습니다.

--- public/js/popularKeywords.js
+++ public/js/popularKeywords.js
@@ -3,19 +3,19 @@ import { RollingKeyword } from "./rollKeywords.js";
 
 
 
-function Search() {
+function PopularKeywords() {
     this.searchBox = document.querySelector(".search_box");
     this.wrapRoll = document.querySelector(".wrap_roll_keywords");
     this.searchInput = document.querySelector(".search_input");
     this.popularSearch = document.querySelector(".popular_search");
     this.popRankUlList = document.querySelectorAll(".pop_rank");
+    this.relContainer = document.querySelector(".rel_search");
     this.rankItems = new RollingKeyword().keywordList;
     this.onEvent();
     this.makeRankList();
-
 }
 
-Search.prototype.onEvent = function () {
+PopularKeywords.prototype.onEvent = function () {
     let timeout;
     this.searchBox.addEventListener("click", this.activateSearch.bind(this));
     this.searchBox.addEventListener("mouseleave", () => {
@@ -26,16 +26,16 @@ Search.prototype.onEvent = function () {
     });
 }
 
-Search.prototype.hide = function (elem) {
+PopularKeywords.prototype.hide = function (elem) {
     elem.classList.add("hide");
 }
 
-Search.prototype.show = function (elem) {
+PopularKeywords.prototype.show = function (elem) {
     elem.classList.remove("hide");
 }
 
 
-Search.prototype.makeRankList = function () {
+PopularKeywords.prototype.makeRankList = function () {
     const itemCount1 = 5;
     const itemCount2 = 10;
     for (let i = 0; i < itemCount1; i++) {
@@ -51,21 +51,25 @@ Search.prototype.makeRankList = function () {
     }
 }
 
-Search.prototype.activateSearch = function () {
+PopularKeywords.prototype.activateSearch = function () {
     this.hide(this.wrapRoll);
     this.show(this.searchInput);
     this.show(this.popularSearch);
-
+    this.show(this.relContainer);
     this.searchInput.focus();
+
+
+    
     this.searchBox.style.border = `red solid 1px`;
 }
 
-Search.prototype.deActivateSearch = function () {
+PopularKeywords.prototype.deActivateSearch = function () {
     this.hide(this.popularSearch);
     this.hide(this.searchInput);
     this.show(this.wrapRoll);
+    this.hide(this.relContainer);
 
     this.searchBox.style.border = `#ececec solid 1px`;
 }
 
-export { Search };
+export { PopularKeywords };
