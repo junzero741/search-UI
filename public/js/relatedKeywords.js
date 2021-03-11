dd95b24004196501c097db42f5553a9a3c21d106
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-03-11 17:58:59 +0900

    Add : 선택 추천 키워드 입력창 반영 기능 추가
    * 검색어를 입력한 뒤 추천 검색어가 뜨면 키보드 방향키 위/아래 로 추천검색어를 선택하고 검색창에 반영하는 기능을 추가했습니다.
    * 그러나 이전에 만들어둔 checkInput 함수가 검색창의 인풋 변화를 감지하고 추천검색어 리스트를 새로 불러와버리는 버그가 존재합니다.
    * 아직 기능 구현에만 초점을 둬서 코드가 많이 더럽습니다.

--- public/js/relatedKeywords.js
+++ public/js/relatedKeywords.js
@@ -5,7 +5,43 @@ function RelatedKeywords() {
     this.popularSearch = document.querySelector(".popular_search");
     this.wrapRoll = document.querySelector(".wrap_roll_keywords");
     this.cache = '';
+    this.listIdx = 0;
+    this.onEvent();
     this.checkInput();
+}
+
+RelatedKeywords.prototype.onEvent = function () {
+    this.searchInput.addEventListener("keydown", (e) => {
+
+        this.bindedPrintKey = this.printKey.bind(this)
+        this.bindedPrintKey(e);
+    });
+}
+
+RelatedKeywords.prototype.printKey = function (e) {
+
+
+    if(this.listIdx > 10) this.listIdx = 10;
+
+    if (e.key === "ArrowDown") {
+        this.searchInput.value = (this.ul.childNodes[this.listIdx].textContent);
+
+        if (this.listIdx === 0) {
+            this.ul.childNodes[this.listIdx].classList.add("selected");
+        } else {
+            this.ul.childNodes[this.listIdx - 1].classList.remove("selected");
+            this.ul.childNodes[this.listIdx].classList.add("selected");
+        }
+        this.listIdx++;
+    }
+
+    if (e.key === "ArrowUp") {
+        this.searchInput.value = (this.ul.childNodes[this.listIdx].textContent);
+        this.ul.childNodes[this.listIdx].classList.remove("selected");
+        this.ul.childNodes[this.listIdx - 1].classList.add("selected");
+
+        this.listIdx--;
+    }
 }
 
 RelatedKeywords.prototype.checkInput = function () {
@@ -19,22 +55,22 @@ RelatedKeywords.prototype.timer = function (beforeInput) {
         if (this.searchInput.value === beforeInput) {
             console.log("입력멈춤");
             this.loadData(this.searchInput.value);
-            this.checkInput();
 
         } else {
             console.log("입력변함 혹은 입력비어있음");
-            this.checkInput();
+        
         }
-
+        this.checkInput();
         this.showHide();
     }, 500);
 }
 
-RelatedKeywords.prototype.showHide = function() {
+RelatedKeywords.prototype.showHide = function () {
     if (this.searchInput.value === "" || this.searchInput !== document.activeElement) {
+        this.listIdx = 0;
         this.relContainer.classList.add("hide");
         this.popularSearch.classList.remove("hide");
-        if(!this.wrapRoll.classList.contains("hide")) this.popularSearch.classList.add("hide");
+        if (!this.wrapRoll.classList.contains("hide")) this.popularSearch.classList.add("hide");
 
     } else {
         this.popularSearch.classList.add("hide");
@@ -47,7 +83,8 @@ RelatedKeywords.prototype.loadData = function (input) {
     const url = `https://completion.amazon.com/api/2017/suggestions?session-id=135-3077052-6015425&customer-id=&request-id=DMRETXPQ3PZJQ5TKYSWX&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=undefined&prefix=${input}&event=onFocusWithSearchTerm&limit=11&fb=1&suggestion-type=KEYWORD&suggestion-type=WIDGET&_=1615280967091`;
 
     if (this.cache === url) return;
-    else {
+    
+    if (this.cache !== url) {
         this.cache = url;
         fetch(url)
             .then((res) => res.json())
@@ -57,25 +94,26 @@ RelatedKeywords.prototype.loadData = function (input) {
 
 
 RelatedKeywords.prototype.fillSearch = function (suggestArr) {
-
+   
     this.ul.innerHTML = "";
-    let pattern = this.searchInput.value;
-    let re = new RegExp(pattern, "g");
+    this.listIdx = 0;
+    const pattern = this.searchInput.value;
+    const re = new RegExp(pattern, "g");
 
     suggestArr.forEach((el) => {
         const li = document.createElement("li");
 
         let searchString = el.value;
         let matchArray;
-        let resultString= "<pre>";
+        let resultString = "<pre>";
         let first = 0;
         let last = 0;
-    
-        while ((matchArray = re.exec(searchString)) != null ) {
+
+        while ((matchArray = re.exec(searchString)) != null) {
             last = matchArray.index;
             resultString += searchString.substring(first, last);
             resultString += "<span class='found'>" + matchArray[0] + "</span>";
-            first = re.lastIndex; 
+            first = re.lastIndex;
         }
         resultString += searchString.substring(first, searchString.length);
         resultString += "</pre>";
@@ -83,6 +121,7 @@ RelatedKeywords.prototype.fillSearch = function (suggestArr) {
         li.innerHTML = resultString;
         this.ul.appendChild(li);
     })
+
 }
 
 export { RelatedKeywords };
