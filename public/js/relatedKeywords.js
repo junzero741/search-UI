53e6c031516e68cc10c112ad088b9232b2abf9dc
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-03-12 21:47:35 +0900

    Fix : 연관검색어 키보드 선택 버그 수정
    * 키보드로 연관검색어를 이동할 때 input 창의 값이 바뀌면 연관검색어 리스트도 새로 업데이트 되는 버그를 수정했습니다.
    * clearTimeout으로 주기적으로 실행되던 함수를 멈추는 방식으로 고쳤습니다.
    * 그랬더니 인풋창에 새로 검색어를 입력해도 인풋을 검사하는 함수가 다시 재실행되지 않는 버그가 생겼습니다.

--- public/js/relatedKeywords.js
+++ public/js/relatedKeywords.js
@@ -6,9 +6,12 @@ function RelatedKeywords() {
     this.wrapRoll = document.querySelector(".wrap_roll_keywords");
     this.cache = '';
     this.listIdx = 0;
+    this.time;
     this.onEvent();
     this.checkInput();
 }
+
+
 
 RelatedKeywords.prototype.onEvent = function () {
     this.searchInput.addEventListener("keydown", (e) => {
@@ -20,10 +23,11 @@ RelatedKeywords.prototype.onEvent = function () {
 
 RelatedKeywords.prototype.printKey = function (e) {
 
-
-    if(this.listIdx > 10) this.listIdx = 10;
+    if (this.listIdx > 10) this.listIdx = 10;
+    // if (this.listIdx < 0) this.listIdx = 1;
 
     if (e.key === "ArrowDown") {
+        clearTimeout(this.time);
         this.searchInput.value = (this.ul.childNodes[this.listIdx].textContent);
 
         if (this.listIdx === 0) {
@@ -33,15 +37,13 @@ RelatedKeywords.prototype.printKey = function (e) {
             this.ul.childNodes[this.listIdx].classList.add("selected");
         }
         this.listIdx++;
-    }
-
-    if (e.key === "ArrowUp") {
-        this.searchInput.value = (this.ul.childNodes[this.listIdx].textContent);
-        this.ul.childNodes[this.listIdx].classList.remove("selected");
+    } else if (e.key === "ArrowUp") {
+        clearTimeout(this.time);
+        this.searchInput.value = (this.ul.childNodes[this.listIdx - 1].textContent);
         this.ul.childNodes[this.listIdx - 1].classList.add("selected");
-
+        this.ul.childNodes[this.listIdx].classList.remove("selected");
         this.listIdx--;
-    }
+    } 
 }
 
 RelatedKeywords.prototype.checkInput = function () {
@@ -51,18 +53,18 @@ RelatedKeywords.prototype.checkInput = function () {
 
 
 RelatedKeywords.prototype.timer = function (beforeInput) {
-    setTimeout(() => {
+    this.time = setTimeout(() => {
         if (this.searchInput.value === beforeInput) {
             console.log("입력멈춤");
             this.loadData(this.searchInput.value);
 
         } else {
             console.log("입력변함 혹은 입력비어있음");
-        
+
         }
         this.checkInput();
         this.showHide();
-    }, 500);
+    }, 1000);
 }
 
 RelatedKeywords.prototype.showHide = function () {
@@ -83,18 +85,18 @@ RelatedKeywords.prototype.loadData = function (input) {
     const url = `https://completion.amazon.com/api/2017/suggestions?session-id=135-3077052-6015425&customer-id=&request-id=DMRETXPQ3PZJQ5TKYSWX&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=undefined&prefix=${input}&event=onFocusWithSearchTerm&limit=11&fb=1&suggestion-type=KEYWORD&suggestion-type=WIDGET&_=1615280967091`;
 
     if (this.cache === url) return;
-    
+
     if (this.cache !== url) {
-        this.cache = url;
         fetch(url)
             .then((res) => res.json())
-            .then((res) => this.fillSearch(res.suggestions));
+            .then((res) => this.fillSearch(res.suggestions))
+            .then(this.cache = url);
     }
 }
 
 
 RelatedKeywords.prototype.fillSearch = function (suggestArr) {
-   
+
     this.ul.innerHTML = "";
     this.listIdx = 0;
     const pattern = this.searchInput.value;
