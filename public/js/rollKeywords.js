c75f154c552e5bdf377428563f186f1806f70bfd
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-08-16 16:35:05 +0900

    refactor: 롤링 검색어 리팩토링 * 상수 이름 대문자로 선언 * 클래스명으로 애니메이션 토글 * 롤링 초기화시점 추상화

--- public/js/rollKeywords.js
+++ public/js/rollKeywords.js
@@ -16,24 +16,28 @@ RollingKeyword.prototype.addList = function () {
 };
 
 RollingKeyword.prototype.rolling = function () {
-  let move = 0;
+  let posY = 0;
+
+  const MOVE_CLASS = 'move';
   const SHOW_TIME = 2000;
   const ROLL_SPACE = 30;
-  const ROLL_END = -330;
+  const rollEnd = -ROLL_SPACE * this.keywordList.length;
+
   const roll = () => {
     setTimeout(() => {
-      this.ol.style.transition = `top 0.3s`;
-      this.ol.style.top = `${move}px`;
-      if (move > ROLL_END) {
-        move -= ROLL_SPACE;
+      this.ol.classList.contains(MOVE_CLASS) || this.ol.classList.add(MOVE_CLASS);
+      this.ol.style.top = `${posY}px`;
+      if (posY > rollEnd) {
+        posY -= ROLL_SPACE;
       } else {
-        this.ol.style.transition = `top 0s`;
-        move = 0;
-        this.ol.style.top = `${move}px`;
+        this.ol.classList.remove(MOVE_CLASS);
+        posY = 0;
+        this.ol.style.top = `${posY}px`;
       }
       roll();
     }, SHOW_TIME);
   };
+
   roll();
 };
 
